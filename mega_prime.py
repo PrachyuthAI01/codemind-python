787d60e5d25b677903ae7d97d374d02deba2f5c8
PRACHYUTHAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-05-06 09:58:29 +0530

    n=int(input()) c=0 r=0 f=0 l=0 for i in range(1,n+1):     if(n%i==0):         c+=1 if(c==2):     while(n):         f=0         d=n%10         r+=1         for x in range(1,d+1):             if(d%x==0):                 f+=1         n=n//10         if(f==2):             l+=1     if(r==l):         print("Mega Prime")     else:         print("Not Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,7 +1,7 @@
 n=int(input())
 c=0
-f=0
 r=0
+f=0
 l=0
 for i in range(1,n+1):
     if(n%i==0):
@@ -11,8 +11,8 @@ if(c==2):
         f=0
         d=n%10
         r+=1
-        for k in range(1,d+1):
-            if(d%k==0):
+        for x in range(1,d+1):
+            if(d%x==0):
                 f+=1
         n=n//10
         if(f==2):
@@ -23,3 +23,4 @@ if(c==2):
         print("Not Mega Prime")
 else:
     print("Not Mega Prime")
+        
