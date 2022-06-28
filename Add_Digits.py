344975b0fd812f5092c21ac3a5500b426ac12578
PRACHYUTHAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-06-28 10:12:53 +0530

    n=int(input()) v=0 while n:     r=n%10     v+=r     n=n//10     if n==0 and v>10:         n=v         v=0 print(v)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,12 +1,10 @@
 n=int(input())
-t=n
-s=0
-while(n):
-    d=n%10
-    s+=d
+v=0
+while n:
+    r=n%10
+    v+=r
     n=n//10
-    if(n==0 and s>9):
-        n=s
-        s=0
-if(s>0 and s<10):
-    print(s)
+    if n==0 and v>10:
+        n=v
+        v=0
+print(v)
