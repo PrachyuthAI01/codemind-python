664ba7fe86c0602278f870f10775b45c9cc3c836
PRACHYUTHAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-06-29 11:21:09 +0530

    v,k=map(int,input().split()) l=0 for i in range(1,v+1 and k+1,1):     if v%i==0 and k%i==0:         l=i print(l)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,10 +1,6 @@
-p,v=map(int,input().split())
-if(p>v):
-    for i in range(1,p):
-        if(p%i==0 and v%i==0):
-            gcd=i
-else:
-    for i in range(1,v):
-        if(p%i==0 and v%i==0):
-            gcd=i
-print(gcd)
+v,k=map(int,input().split())
+l=0
+for i in range(1,v+1 and k+1,1):
+    if v%i==0 and k%i==0:
+        l=i
+print(l)
