dd678660fb6a4932e1257f4f3440002eaf074ac5
PRACHYUTHAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-06-29 11:16:10 +0530

    import math n=int(input()) s=int(math.sqrt(n)) for i in range(1,s+1):     if(i*i==n):         print(True)         break else:     print(False)

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,11 +1,9 @@
-v=int(input())
-p=1
-i=0
-for p in range(v):
-    r=p*p
-    if(r==v):
-        i+=1
-if(i==1):
-    print("True")
+import math
+n=int(input())
+s=int(math.sqrt(n))
+for i in range(1,s+1):
+    if(i*i==n):
+        print(True)
+        break
 else:
-    print("False")
+    print(False)
